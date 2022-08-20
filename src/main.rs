434dfcb46423af457ba7d69097d9882d33062a15
andrej <andrej.janda@mail.utoronto.ca>
2022-08-20 12:21:23 -0400

    Computes time steps

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use matfile;
-// use ndarray::Array2;
-use ndarray::ArrayD;
 use ndarray::IxDyn;
+use ndarray::{Array1, Array2, ArrayD};
 use std;
 use std::convert::TryInto;
 use std::error;
@@ -56,8 +55,8 @@ struct BatchEstimator {
     cv: f64,
     b: f64,
     num_points: i32,
-    time_steps: ArrayD<f64>,
-    t_c_v: ArrayD<f64>,
+    time_steps: Array1<f64>,
+    t_c_v: Array2<f64>,
 }
 
 impl BatchEstimator {
@@ -66,6 +65,7 @@ impl BatchEstimator {
         let file = std::fs::File::open(filename)?;
         let mat_file = matfile::MatFile::parse(file)?;
 
+        // loading individual elements
         fn load(mat_file: &matfile::MatFile, name: &str) -> Result<ArrayD<f64>, EstimationError> {
             let arr = match mat_file.find_by_name(name) {
                 Some(arr) => arr,
@@ -78,7 +78,8 @@ impl BatchEstimator {
             Ok(arr)
         }
 
-        Ok(BatchEstimator {
+        // load each field
+        let mut batch_estimator = BatchEstimator {
             rho_v_c_v: load(&mat_file, "rho_v_c_v")?,
             rho_i_pj_i: load(&mat_file, "rho_i_pj_i")?,
             y_k_j: load(&mat_file, "y_k_j")?,
@@ -92,14 +93,25 @@ impl BatchEstimator {
             w_var: load(&mat_file, "w_var")?,
             y_var: load(&mat_file, "y_var")?,
             num_points: 20,
-            time_steps: ArrayD::zeros(IxDyn(&[1])),
-            t_c_v: ArrayD::zeros(IxDyn(&[1])),
+            time_steps: Array1::zeros(1900),
+            t_c_v: Array2::zeros([4, 4]),
             fu: load(&mat_file, "fu")?[[0, 0]],
             fv: load(&mat_file, "fv")?[[0, 0]],
             cu: load(&mat_file, "cu")?[[0, 0]],
             cv: load(&mat_file, "cv")?[[0, 0]],
             b: load(&mat_file, "b")?[[0, 0]],
-        })
+        };
+
+        // compute time step difference
+        for i in 1..1900 {
+            batch_estimator.time_steps[[i]] =
+                batch_estimator.t[[0, i]] - batch_estimator.t[[0, i - 1]];
+        }
+
+        // make camera to vehicle transformation matrix
+        t_c_v;
+
+        Ok(batch_estimator)
     }
 }
 
