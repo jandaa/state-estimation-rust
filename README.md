8a7bb52f9dfec9f736ccfe543112e1f20c7647b8
andrej <andrej.janda@mail.utoronto.ca>
2022-08-20 11:24:41 -0400

    Reads in data

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # state-estimation-rust
-Re-implementing a batch state estimator in rust.
+
+This project contains re-implementations of an assignment I did for my state estimation course. I originally wrote it in Python and re-implement it in rust for the purposes of exploring the language. 
+
+For more details, please visit my [project page](http://www.ajanda.ca/projects/rust)
+
+# Setup
+
+This setup assumes you already have python 3.8 or greater and rust already installed.
+## Download data
+
+First, we need to download the dataset used for the project. To do this on a unix system, use:
+
+```bash
+wget https://files.ajanda.ca/state-estimation/data.mat
+```
---
+++ src/main.rs
@@ -0,0 +1,109 @@
+use matfile;
+// use ndarray::Array2;
+use ndarray::ArrayD;
+use ndarray::IxDyn;
+use std;
+use std::convert::TryInto;
+use std::error;
+use std::fmt;
+
+// Custom Estimation Error
+#[derive(PartialEq, Debug)]
+enum EstimationError {
+    FileNotFound,
+    ParseError,
+}
+
+impl fmt::Display for EstimationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let description = match *self {
+            EstimationError::FileNotFound => "File not found",
+            EstimationError::ParseError => "Parse Error",
+        };
+        f.write_str(description)
+    }
+}
+
+impl std::convert::From<std::io::Error> for EstimationError {
+    fn from(_: std::io::Error) -> Self {
+        EstimationError::FileNotFound
+    }
+}
+impl std::convert::From<matfile::Error> for EstimationError {
+    fn from(_: matfile::Error) -> Self {
+        EstimationError::ParseError
+    }
+}
+
+impl error::Error for EstimationError {}
+
+struct BatchEstimator {
+    rho_v_c_v: ArrayD<f64>,
+    rho_i_pj_i: ArrayD<f64>,
+    y_k_j: ArrayD<f64>,
+    C_c_v: ArrayD<f64>,
+    theta_vk_i: ArrayD<f64>,
+    r_i_vk_i: ArrayD<f64>,
+    t: ArrayD<f64>,
+    w_vk_vk_i: ArrayD<f64>,
+    v_vk_vk_i: ArrayD<f64>,
+    v_var: ArrayD<f64>,
+    w_var: ArrayD<f64>,
+    y_var: ArrayD<f64>,
+    fu: f64,
+    fv: f64,
+    cu: f64,
+    cv: f64,
+    b: f64,
+    num_points: i32,
+    time_steps: ArrayD<f64>,
+    t_c_v: ArrayD<f64>,
+}
+
+impl BatchEstimator {
+    fn new(filename: &str) -> Result<Self, EstimationError> {
+        // Read in the file
+        let file = std::fs::File::open(filename)?;
+        let mat_file = matfile::MatFile::parse(file)?;
+
+        fn load(mat_file: &matfile::MatFile, name: &str) -> Result<ArrayD<f64>, EstimationError> {
+            let arr = match mat_file.find_by_name(name) {
+                Some(arr) => arr,
+                None => return Err(EstimationError::ParseError),
+            };
+            let arr: ArrayD<f64> = match arr.try_into() {
+                Ok(arr) => arr,
+                Err(_) => return Err(EstimationError::ParseError),
+            };
+            Ok(arr)
+        }
+
+        Ok(BatchEstimator {
+            rho_v_c_v: load(&mat_file, "rho_v_c_v")?,
+            rho_i_pj_i: load(&mat_file, "rho_i_pj_i")?,
+            y_k_j: load(&mat_file, "y_k_j")?,
+            C_c_v: load(&mat_file, "C_c_v")?,
+            theta_vk_i: load(&mat_file, "theta_vk_i")?,
+            r_i_vk_i: load(&mat_file, "r_i_vk_i")?,
+            t: load(&mat_file, "t")?,
+            w_vk_vk_i: load(&mat_file, "w_vk_vk_i")?,
+            v_vk_vk_i: load(&mat_file, "v_vk_vk_i")?,
+            v_var: load(&mat_file, "v_var")?,
+            w_var: load(&mat_file, "w_var")?,
+            y_var: load(&mat_file, "y_var")?,
+            num_points: 20,
+            time_steps: ArrayD::zeros(IxDyn(&[1])),
+            t_c_v: ArrayD::zeros(IxDyn(&[1])),
+            fu: load(&mat_file, "fu")?[[0, 0]],
+            fv: load(&mat_file, "fv")?[[0, 0]],
+            cu: load(&mat_file, "cu")?[[0, 0]],
+            cv: load(&mat_file, "cv")?[[0, 0]],
+            b: load(&mat_file, "b")?[[0, 0]],
+        })
+    }
+}
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let batch_estimator = BatchEstimator::new("data.mat")?;
+    Ok(())
+}
